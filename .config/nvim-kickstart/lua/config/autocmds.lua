d815dc686ad1e603fa0158211585d829aead3fb2
Florian Yvan <34156361+yvanflorian@users.noreply.github.com>
2026-01-09 14:05:52 +0300

    markdown auto spell-check

--- .config/nvim-kickstart/lua/config/autocmds.lua
+++ .config/nvim-kickstart/lua/config/autocmds.lua
@@ -54,3 +54,16 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		-- vim.lsp.buf.format({async = false})
 	end,
 })
+
+--
+--
+-- For markdown, set wrap by default and spellcheck
+-- for en_us
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_us" -- or "en_gb", etc.
+	end,
+})
